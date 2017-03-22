84766ec52fcaa4242d8aba3cea342f81474350ad
David Immanuel <daweiimmanuel@users.noreply.github.com>
2017-03-22 19:12:25 +0700

    Add reverse-string.js

---
+++ phase-0/week-2/day-3/reverse-string.js
@@ -0,0 +1,11 @@
+function balikString(str){
+    var reversedString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return reversedString;
+}
+
+var kata = prompt('Input kata');
+
+console.log(balikString(kata));
